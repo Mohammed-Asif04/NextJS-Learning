eb36ef149ef7fca605fa90261132225edaf323ae
Mohammed-Asif04 <mohad.asif0407@gmail.com>
2025-08-08 23:55:06 +0530

    Update

--- snippets/src/app/snippet/[id]/edit/page.tsx
+++ snippets/src/app/snippet/[id]/edit/page.tsx
@@ -2,7 +2,7 @@
 import EditSnippetForm from '@/components/EditSnippetForm';
 import { prisma } from '@/lib/prisma';
 import { notFound } from 'next/navigation';
-import { useEffect } from 'react';
+import React from 'react';
 
 const EditSnippetPage = async({params}: {params: Promise<{id: string}>}) => {
     const id = parseInt((await params).id);
